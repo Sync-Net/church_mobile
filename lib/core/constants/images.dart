2d87f4dea66e5b46b08782351fdf0c6e380fb68c
fady emil <fady46t45f@gmail.com>
2024-11-25 11:36:28 +0200

    Add SVG image assets and update pubspec.yaml
    Added a new file `images.dart` to store SVG image asset paths and updated `pubspec.yaml` to include the new assets directory. Also, added configuration for generating the assets file.

---
+++ lib/core/constants/images.dart
@@ -0,0 +1,130 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imageIconsSVGBookmark
+  /// assets/image/icons_SVG/bookmark.svg
+  static const String imageIconsSVGBookmark =
+      "assets/image/icons_SVG/bookmark.svg";
+
+  /// Assets for imageIconsSVGChat
+  /// assets/image/icons_SVG/chat.svg
+  static const String imageIconsSVGChat = "assets/image/icons_SVG/chat.svg";
+
+  /// Assets for imageIconsSVGCrown
+  /// assets/image/icons_SVG/crown.svg
+  static const String imageIconsSVGCrown = "assets/image/icons_SVG/crown.svg";
+
+  /// Assets for imageIconsSVGDropdownArrowIcon
+  /// assets/image/icons_SVG/Dropdown-Arrow-Icon.svg
+  static const String imageIconsSVGDropdownArrowIcon =
+      "assets/image/icons_SVG/Dropdown-Arrow-Icon.svg";
+
+  /// Assets for imageIconsSVGErrorIcon
+  /// assets/image/icons_SVG/Error-Icon.svg
+  static const String imageIconsSVGErrorIcon =
+      "assets/image/icons_SVG/Error-Icon.svg";
+
+  /// Assets for imageIconsSVGGroup
+  /// assets/image/icons_SVG/Group.svg
+  static const String imageIconsSVGGroup = "assets/image/icons_SVG/Group.svg";
+
+  /// Assets for imageIconsSVGHeart
+  /// assets/image/icons_SVG/heart.svg
+  static const String imageIconsSVGHeart = "assets/image/icons_SVG/heart.svg";
+
+  /// Assets for imageIconsSVGHidePasswordIcon
+  /// assets/image/icons_SVG/Hide-Password-Icon.svg
+  static const String imageIconsSVGHidePasswordIcon =
+      "assets/image/icons_SVG/Hide-Password-Icon.svg";
+
+  /// Assets for imageIconsSVGHome
+  /// assets/image/icons_SVG/home.svg
+  static const String imageIconsSVGHome = "assets/image/icons_SVG/home.svg";
+
+  /// Assets for imageIconsSVGHugeiconsChurch
+  /// assets/image/icons_SVG/hugeicons_church.svg
+  static const String imageIconsSVGHugeiconsChurch =
+      "assets/image/icons_SVG/hugeicons_church.svg";
+
+  /// Assets for imageIconsSVGIconCarrier
+  /// assets/image/icons_SVG/iconCarrier.svg
+  static const String imageIconsSVGIconCarrier =
+      "assets/image/icons_SVG/iconCarrier.svg";
+
+  /// Assets for imageIconsSVGInformationIcon
+  /// assets/image/icons_SVG/Information-Icon.svg
+  static const String imageIconsSVGInformationIcon =
+      "assets/image/icons_SVG/Information-Icon.svg";
+
+  /// Assets for imageIconsSVGKeySquare
+  /// assets/image/icons_SVG/key-square.svg
+  static const String imageIconsSVGKeySquare =
+      "assets/image/icons_SVG/key-square.svg";
+
+  /// Assets for imageIconsSVGLockPassword
+  /// assets/image/icons_SVG/lock-password.svg
+  static const String imageIconsSVGLockPassword =
+      "assets/image/icons_SVG/lock-password.svg";
+
+  /// Assets for imageIconsSVGMiSearch
+  /// assets/image/icons_SVG/mi_search.svg
+  static const String imageIconsSVGMiSearch =
+      "assets/image/icons_SVG/mi_search.svg";
+
+  /// Assets for imageIconsSVGReply
+  /// assets/image/icons_SVG/reply.svg
+  static const String imageIconsSVGReply = "assets/image/icons_SVG/reply.svg";
+
+  /// Assets for imageIconsSVGSVGRepoIconCarrier
+  /// assets/image/icons_SVG/SVGRepo_iconCarrier.svg
+  static const String imageIconsSVGSVGRepoIconCarrier =
+      "assets/image/icons_SVG/SVGRepo_iconCarrier.svg";
+
+  /// Assets for imageIconsSVGSearchIcon
+  /// assets/image/icons_SVG/Search-Icon.svg
+  static const String imageIconsSVGSearchIcon =
+      "assets/image/icons_SVG/Search-Icon.svg";
+
+  /// Assets for imageIconsSVGShowPasswordIcon
+  /// assets/image/icons_SVG/Show-Password-Icon.svg
+  static const String imageIconsSVGShowPasswordIcon =
+      "assets/image/icons_SVG/Show-Password-Icon.svg";
+
+  /// Assets for imageIconsSVGSort
+  /// assets/image/icons_SVG/sort.svg
+  static const String imageIconsSVGSort = "assets/image/icons_SVG/sort.svg";
+
+  /// Assets for imageIconsSVGStar
+  /// assets/image/icons_SVG/star.svg
+  static const String imageIconsSVGStar = "assets/image/icons_SVG/star.svg";
+
+  /// Assets for imageIconsSVGSuccessIcon
+  /// assets/image/icons_SVG/Success-Icon.svg
+  static const String imageIconsSVGSuccessIcon =
+      "assets/image/icons_SVG/Success-Icon.svg";
+
+  /// Assets for imageIconsSVGUndoRight
+  /// assets/image/icons_SVG/undo-right.svg
+  static const String imageIconsSVGUndoRight =
+      "assets/image/icons_SVG/undo-right.svg";
+
+  /// Assets for imageIconsSVGUser
+  /// assets/image/icons_SVG/user.svg
+  static const String imageIconsSVGUser = "assets/image/icons_SVG/user.svg";
+
+  /// Assets for imageIconsSVGUserCheck
+  /// assets/image/icons_SVG/user-check.svg
+  static const String imageIconsSVGUserCheck =
+      "assets/image/icons_SVG/user-check.svg";
+
+  /// Assets for imageIconsSVGUserMinus
+  /// assets/image/icons_SVG/user-minus.svg
+  static const String imageIconsSVGUserMinus =
+      "assets/image/icons_SVG/user-minus.svg";
+
+  /// Assets for imageIconsSVGUserPlus
+  /// assets/image/icons_SVG/user-plus.svg
+  static const String imageIconsSVGUserPlus =
+      "assets/image/icons_SVG/user-plus.svg";
+}
