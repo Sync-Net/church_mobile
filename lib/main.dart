9d1b59c7ddfd35f6bf72029e6fc4e649dbeb8699
fady emil <fady46t45f@gmail.com>
2024-11-25 11:54:51 +0200

    Hide debug banner and add Cairo font
    Updated the `MyApp` widget to hide the debug banner and configured the `Cairo` font in the `pubspec.yaml` file, including various font weights.

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        fontFamily: 'Cairo',
       ),
       routerConfig: router,
     );
