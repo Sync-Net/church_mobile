4700776e7274a480aec9a6962d587863d5866aaa
fady emil <fady46t45f@gmail.com>
2024-11-25 14:19:04 +0200

    Add comprehensive text styles for app theme
    Added a variety of text styles with different font weights and sizes to the `AppTextStyles` class. This includes bold, semi-bold, medium, regular, and light styles for titles and body text, ensuring consistent typography throughout the application.

--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -1,3 +1,266 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class AppTextStyles {
-  
+  //***************************Bold***********************
+
+  static TextStyle titleBold48 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 48.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle titleBold40 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 40.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle titleBold32 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 32.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyBold24 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 24.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyBold20 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 20.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyBold18 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 18.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyBold14 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 14.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyBold12 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 12.sp,
+    color: Colors.black,
+  );
+
+//***************************Semi Bold*****************************
+
+  static TextStyle titleSemiBold40 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 40.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle titleSemiBold36 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 36.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle titleSemiBold28 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 28.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodySemiBold24 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 24.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodySemiBold20 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodySemiBold18 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 18.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodySemiBold16 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodySemiBold14 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 14.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodySemiBold12 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 12.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle titleSemiBold11 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 11.sp,
+    color: Colors.black,
+  );
+
+  // *************************** Medium **********************
+
+  static TextStyle titleMedium32 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 32.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyMedium26 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 26.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyMedium24 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 24.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyMedium20 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 20.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyMedium18 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 18.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyMedium16 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyMedium14 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 14.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyMedium12 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 12.sp,
+    color: Colors.black,
+  );
+
+// *****************Regular*********************
+
+  static TextStyle titleRegular32 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 32.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyRegular26 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 26.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyRegular24 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 24.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyRegular20 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 20.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyRegular18 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 18.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyRegular16 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 16.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyRegular14 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 14.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyRegular12 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 12.sp,
+    color: Colors.black,
+  );
+
+  // *****************Light*********************
+
+  static TextStyle titleLight32 = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 32.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyLight26 = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 26.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyLight24 = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 24.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyLight20 = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 20.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyLight18 = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 18.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyLight16 = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 16.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyLight14 = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 14.sp,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyLight12 = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 12.sp,
+    color: Colors.black,
+  );
 }
