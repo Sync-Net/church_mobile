973c949234c4ef61d0fb0202e92538d4d32b1ae3
fady emil <fady46t45f@gmail.com>
2024-11-25 16:08:31 +0200

    Add color definitions for app theme
    Added detailed color definitions for various themes including baby blue, soft sky blue, dark slate blue, golden sand, scarlet red, and soft moss green. Each color set includes shades from 50 to 950 for consistent theming across the application.

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,3 +1,82 @@
+import 'package:flutter/services.dart';
+
 class AppColors {
-  
+  // ************************baby blue*************************
+  static const Color background500 = Color(0xFFD9EFF7); //~ Base color
+  static const Color background50 = Color(0xFFEAF6FB);
+  static const Color background100 = Color(0xFFD5EDF6);
+  static const Color background200 = Color(0xFFABDCED);
+  static const Color background300 = Color(0xFF81CAE4);
+  static const Color background400 = Color(0xFF56B8DC);
+  static const Color background600 = Color(0xFF2385A9);
+  static const Color background700 = Color(0xFF1B647E);
+  static const Color background800 = Color(0xFF124354);
+  static const Color background900 = Color(0xFF09212A);
+  static const Color background950 = Color(0xFF041115);
+
+  // ************************soft sky blue*************************
+  static const Color set500 = Color(0xFF9BBBFC); //~ Base color
+  static const Color set50 = Color(0xFFE6EEFE);
+  static const Color set100 = Color(0xFFCDDDFE);
+  static const Color set200 = Color(0xFF9CBCFC);
+  static const Color set300 = Color(0xFF6A9AFB);
+  static const Color set400 = Color(0xFF3979F9);
+  static const Color set600 = Color(0xFF0646C6);
+  static const Color set700 = Color(0xFF043495);
+  static const Color set800 = Color(0xFF032363);
+  static const Color set900 = Color(0xFF011132);
+  static const Color set950 = Color(0xFF010919);
+
+  // ************************Dark Slate Blue*************
+  static const Color main500 = Color(0xFF4841A6); //~ Base color
+  static const Color main50 = Color(0xFFEDEDF8);
+  static const Color main100 = Color(0xFFDCD9F1);
+  static const Color main200 = Color(0xFFB9B6E2);
+  static const Color main300 = Color(0xFF9691D4);
+  static const Color main400 = Color(0xFF726CC6);
+  static const Color main600 = Color(0xFF3F3993);
+  static const Color main700 = Color(0xFF302B6E);
+  static const Color main800 = Color(0xFF201D49);
+  static const Color main900 = Color(0xFF100E25);
+  static const Color main950 = Color(0xFF080712);
+
+  // ******************* Golden Sand********************
+  static const Color button500 = Color(0xFFF9CF69); //~ Base color
+  static const Color button50 = Color(0xFFFFF7E6);
+  static const Color button100 = Color(0xFFFEDFCF);
+  static const Color button200 = Color(0xFFFBD9D9);
+  static const Color button300 = Color(0xFFF9CFC6);
+  static const Color button400 = Color(0xFFF7BF3B);
+  static const Color button600 = Color(0xFFC4B808);
+  static const Color button700 = Color(0xFF936906);
+  static const Color button800 = Color(0xFF624604);
+  static const Color button900 = Color(0xFF312302);
+  static const Color button950 = Color(0xFF191101);
+
+  // ******************  scarlet red***********************
+  static const Color error500 = Color(0xFFFA3F32); //~ Base color
+  static const Color error50 = Color(0xFFFEE8E6);
+  static const Color error100 = Color(0xFFFED0CD);
+  static const Color error200 = Color(0xFFFDA29B);
+  static const Color error300 = Color(0xFFFB736A);
+  static const Color error400 = Color(0xFFFA4538);
+  static const Color error600 = Color(0xFFC71205);
+  static const Color error700 = Color(0xFF950D04);
+  static const Color error800 = Color(0xFF640902);
+  static const Color error900 = Color(0xFF320401);
+  static const Color error950 = Color(0xFF190201);
+
+  // *******************  Soft moss green********************
+
+  static const Color success500 = Color(0xFF88C273); //~ Base color
+  static const Color success50 = Color(0xFFF0F7ED);
+  static const Color success100 = Color(0xFFE1F0DB);
+  static const Color success200 = Color(0xFFC3E0B8);
+  static const Color success300 = Color(0xFFA4D194);
+  static const Color success400 = Color(0xFF86C171);
+  static const Color success600 = Color(0xFF538E3E);
+  static const Color success700 = Color(0xFF3E6B2E);
+  static const Color success800 = Color(0xFF2A471F);
+  static const Color success900 = Color(0xFF15240F);
+  static const Color success950 = Color(0xFF0A1208);
 }
