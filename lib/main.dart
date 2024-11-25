c68bf02d5a0c099700130e2fb04dfc745bf27929
fady emil <fady46t45f@gmail.com>
2024-11-25 12:20:25 +0200

    Add localization support and configure flutter_intl
    Added the necessary imports for localization and configured the `flutter_intl` package in `pubspec.yaml`. Updated the `MyApp` widget to include `localizationsDelegates` and `supportedLocales` for Arabic language support.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:church_mobile/core/router/router.dart';
 import 'package:church_mobile/core/services/cache_helper.dart';
 import 'package:church_mobile/core/services/custom_bloc_server.dart';
 import 'package:church_mobile/firebase_options.dart';
+import 'package:church_mobile/generated/l10n.dart';
 import 'package:church_mobile/hive_setup.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:go_router/go_router.dart';
 
 Future<void> main() async {
@@ -30,6 +32,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('ar'),
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
